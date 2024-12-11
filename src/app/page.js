97a3e251a11b88d973748283ad99d5132b382bee
Fırat Kahvecioğlu <firatkahvecioglu@Frats-MacBook-Pro.local>
2024-12-11 19:38:49 +0300

    delete about

--- src/app/page.js
+++ src/app/page.js
@@ -14,7 +14,7 @@ export default function Home() {
       <div className="container mt-24 mx-auto px-12 py-4">
         <HeroSection />
         <AboutSection />
-        <Hakkimda />
+        {/* <Hakkimda /> */}
         {/* <ProjectsSection /> */}
       </div>
       <Footer />
